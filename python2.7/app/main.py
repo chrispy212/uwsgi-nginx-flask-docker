6aab2cb371f2a44decea2fdcd499fca6734edc26
chrispy212 <christianlundpetersen@gmail.com>
2019-07-03 18:38:25 +0100

    Add App Files

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -1,18 +1,22 @@
-import sys
+from flask import render_template
+import connexion
 
-from flask import Flask
+# Create the application instance
+app = connexion.App(__name__, specification_dir='./')
 
-app = Flask(__name__)
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
 
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
 
-@app.route("/")
-def hello():
-    version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    message = "Hello World from Flask in a uWSGI Nginx Docker container with Python {} (default)".format(
-        version
-    )
-    return message
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
